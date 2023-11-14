96267bc3275102d8e7160a2823eb94ef9731f847
vaibhav sharma <vaibhav.sharma@embifi.in>
2023-11-14 22:32:36 +0530

    base url changes

--- src/config/DBConfig.js
+++ src/config/DBConfig.js
@@ -10,14 +10,14 @@ const getDBURI = () =>{
 // mongodb+srv://vaibhavsharamofficial31:6MyNm7u4L1gUtUhe@cluster0.bxqnucs.mongodb.net/?retryWrites=true&w=majority
 
 const DBConnection = () =>{
+    console.log("Enviroment :-" ,process.env.ENVIROMENT)
+    console.log("Base Url :-",getDBURI())
     mongoose.connect(getDBURI() , {
         useNewUrlParser:true,
         useUnifiedTopology:true,
     }).then((data)=>{
-        console.log("Base Url :-",getDBURI())
         console.log(`... MongoDB is connnected with server ${data.connection.host}...`)
     }).catch((err)=>{
-        console.log("Base Url :-",getDBURI())
         console.log(`... MongoDB is not connected and the error is : ${err} ...`)
     })
 }  
